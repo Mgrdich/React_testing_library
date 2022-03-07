30d2984eb45368d89b38124f1d57838c1685772c
Mgrdich <mgotm13@gmail.com>
2022-03-07 23:02:46 +0400

    Code testing modification in the header

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -7,15 +7,43 @@ test('should render the same text', () => {
     expect(headingElement).toBeInTheDocument();
 });
 
-test('should render the same text by another Selector method', () => {
-    render(<Header title="myElement"/>);
-    let headingElement = screen.getByRole('heading', {name:"myElement"});
+// test('should render the same text by another Selector method', () => {
+//     render(<Header title="myElement"/>);
+//     // let headingElement = screen.getByRole('heading', {name:/myElement/i});
+//     let headingElement = screen.getByRole('heading', {name:"myElement"});
+//     expect(headingElement).toBeInTheDocument();
+// });
+
+test('should render the same text by another get Method', () => {
+    render(<Header title="Todo"/>);
+    let headingElement = screen.getByTitle('header');
     expect(headingElement).toBeInTheDocument();
 });
 
 test('should render the same text by another get Method', () => {
     render(<Header title="Todo"/>);
-    let headingElement = screen.getByTitle('header')
+    let headingElement = screen.getByTestId('Header_Element');
     expect(headingElement).toBeInTheDocument();
 });
 
+// Find by syntax
+
+test('should render the same text with Find By syntax', async () => {
+    render(<Header title="Todo"/>);
+    let headingElement = await screen.findByText(/todo/i);
+    expect(headingElement).toBeInTheDocument();
+});
+
+//Query By Syntax
+
+test('should render the same text with query by syntax', () => {
+    render(<Header title="Todo"/>);
+    let headingElement = screen.queryByText(/todo/i);
+    expect(headingElement).toBeInTheDocument();
+});
+
+test('should render the same text with query by syntax', () => {
+    render(<Header title="Todo"/>);
+    let headingElement = screen.queryByText(/todos/i);
+    expect(headingElement).not.toBeInTheDocument();
+});
