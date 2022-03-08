47a5491399e7a16c4fb51190d4316165edbb0cb0
Mgrdich <mgotm13@gmail.com>
2022-03-08 12:38:13 +0400

    testing files for TodoFooter seperation with individual describe blocks

--- src/components/TodoFooter/__tests__/TodoFooter.test.js
+++ src/components/TodoFooter/__tests__/TodoFooter.test.js
@@ -2,9 +2,6 @@ import {render, screen} from '@testing-library/react';
 import TodoFooter from "../TodoFooter";
 import {BrowserRouter} from "react-router-dom";
 
-
-
-
 describe('TodoFooter', () => {
     /**
      * @param num {Number}
@@ -17,47 +14,44 @@ describe('TodoFooter', () => {
         );
     }
 
-    describe('TodoFooter with multiple Tasks' , ()=> {
+    describe('TodoFooter with multiple Tasks', () => {
+        beforeEach(() => setUpRenderTodoFooter(5));
+
         it("it should render show that element exist", () => {
-            setUpRenderTodoFooter(5);
             let element = screen.getByTestId("task_number_indicator");
             expect(element).toBeInTheDocument();
         });
 
         it("it should view correct number of tasks", () => {
-            setUpRenderTodoFooter(5);
             let element = screen.getByText(/5 tasks left/i);
             expect(element).toBeInTheDocument();
         });
     });
 
     describe('TodoFooter with single Task', () => {
+        beforeEach(() => setUpRenderTodoFooter(1));
+
         it("it should view correct number of task", () => {
-            setUpRenderTodoFooter(1);
             let element = screen.getByText(/1 task left/i);
             expect(element).toBeInTheDocument();
         });
 
         it("it should view should be visible in the Document", () => {
-            setUpRenderTodoFooter(1);
             let element = screen.getByText(/1 task left/i);
             expect(element).toBeVisible();
         });
 
         it("it should view should contain in the Document", () => {
-            setUpRenderTodoFooter(1);
             let element = screen.getByText(/1 task left/i);
             expect(element).toContainHTML('p');
         });
 
         it("it should view should contain in the Document", () => {
-            setUpRenderTodoFooter(1);
             let element = screen.getByTestId("task_number_indicator");
             expect(element).toHaveTextContent('1 task left');
         });
 
         it("it should view should contain in the Document", () => {
-            setUpRenderTodoFooter(1);
             let element = screen.getByTestId("task_number_indicator");
             expect(element.textContent).toBe('1 task left');
         });
