f77c881d083c724a7830d255da6124757ccbdb3d
Mgrdich <mgotm13@gmail.com>
2022-03-08 15:54:56 +0400

    adding async testing for the followers

--- src/components/FollowersList/__tests__/FollowersList.test.js
+++ src/components/FollowersList/__tests__/FollowersList.test.js
@@ -13,7 +13,14 @@ describe('FollowersList', () => {
 
     beforeEach(() => setUpFollowersList());
 
-    it('should view the component correctly', function () {
-        
+    it('should view the component and its first element correctly', async () => {
+        let firstFollowerElement = await screen.findByTestId('follower-item-0');
+        expect(firstFollowerElement).toBeInTheDocument();
+    });
+
+
+    it('should view the component and all its children', async () => {
+        let firstFollowerElements = await screen.findAllByTestId(/follower-item/i);
+        expect(firstFollowerElements.length).toBe(5);
     });
 });
