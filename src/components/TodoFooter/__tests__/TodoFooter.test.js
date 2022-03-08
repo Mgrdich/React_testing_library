8f81a11706fb7368228f954f480e80840876f186
Mgrdich <mgotm13@gmail.com>
2022-03-08 12:23:12 +0400

    testing files for TodoFooter

--- src/components/TodoFooter/__tests__/TodoFooter.test.js
+++ src/components/TodoFooter/__tests__/TodoFooter.test.js
@@ -34,4 +34,28 @@ describe('TodoFooter', () => {
         let element = screen.getByText(/1 task left/i);
         expect(element).toBeInTheDocument();
     });
+
+    it("it should view should be visible in the Document", () => {
+        setUpRenderTodoFooter(1);
+        let element = screen.getByText(/1 task left/i);
+        expect(element).toBeVisible();
+    });
+
+    it("it should view should contain in the Document", () => {
+        setUpRenderTodoFooter(1);
+        let element = screen.getByText(/1 task left/i);
+        expect(element).toContainHTML('p');
+    });
+
+    it("it should view should contain in the Document", () => {
+        setUpRenderTodoFooter(1);
+        let element = screen.getByTestId("task_number_indicator");
+        expect(element).toHaveTextContent('1 task left');
+    });
+
+    it("it should view should contain in the Document", () => {
+        setUpRenderTodoFooter(1);
+        let element = screen.getByTestId("task_number_indicator");
+        expect(element.textContent).toBe('1 task left');
+    });
 });
