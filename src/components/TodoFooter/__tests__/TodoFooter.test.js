8425ca54b23a6c0e0bae35796aff94f0bd58654f
Mgrdich <mgotm13@gmail.com>
2022-03-08 12:35:02 +0400

    testing files for TodoFooter seperation with individual describe blocks

--- src/components/TodoFooter/__tests__/TodoFooter.test.js
+++ src/components/TodoFooter/__tests__/TodoFooter.test.js
@@ -17,45 +17,49 @@ describe('TodoFooter', () => {
         );
     }
 
-    it("it should render show that element exist", () => {
-        setUpRenderTodoFooter(5);
-        let element = screen.getByTestId("task_number_indicator");
-        expect(element).toBeInTheDocument();
+    describe('TodoFooter with multiple Tasks' , ()=> {
+        it("it should render show that element exist", () => {
+            setUpRenderTodoFooter(5);
+            let element = screen.getByTestId("task_number_indicator");
+            expect(element).toBeInTheDocument();
+        });
+
+        it("it should view correct number of tasks", () => {
+            setUpRenderTodoFooter(5);
+            let element = screen.getByText(/5 tasks left/i);
+            expect(element).toBeInTheDocument();
+        });
     });
 
-    it("it should view correct number of tasks", () => {
-        setUpRenderTodoFooter(5);
-        let element = screen.getByText(/5 tasks left/i);
-        expect(element).toBeInTheDocument();
-    });
+    describe('TodoFooter with single Task', () => {
+        it("it should view correct number of task", () => {
+            setUpRenderTodoFooter(1);
+            let element = screen.getByText(/1 task left/i);
+            expect(element).toBeInTheDocument();
+        });
 
-    it("it should view correct number of task", () => {
-        setUpRenderTodoFooter(1);
-        let element = screen.getByText(/1 task left/i);
-        expect(element).toBeInTheDocument();
-    });
+        it("it should view should be visible in the Document", () => {
+            setUpRenderTodoFooter(1);
+            let element = screen.getByText(/1 task left/i);
+            expect(element).toBeVisible();
+        });
 
-    it("it should view should be visible in the Document", () => {
-        setUpRenderTodoFooter(1);
-        let element = screen.getByText(/1 task left/i);
-        expect(element).toBeVisible();
-    });
+        it("it should view should contain in the Document", () => {
+            setUpRenderTodoFooter(1);
+            let element = screen.getByText(/1 task left/i);
+            expect(element).toContainHTML('p');
+        });
 
-    it("it should view should contain in the Document", () => {
-        setUpRenderTodoFooter(1);
-        let element = screen.getByText(/1 task left/i);
-        expect(element).toContainHTML('p');
-    });
+        it("it should view should contain in the Document", () => {
+            setUpRenderTodoFooter(1);
+            let element = screen.getByTestId("task_number_indicator");
+            expect(element).toHaveTextContent('1 task left');
+        });
 
-    it("it should view should contain in the Document", () => {
-        setUpRenderTodoFooter(1);
-        let element = screen.getByTestId("task_number_indicator");
-        expect(element).toHaveTextContent('1 task left');
-    });
-
-    it("it should view should contain in the Document", () => {
-        setUpRenderTodoFooter(1);
-        let element = screen.getByTestId("task_number_indicator");
-        expect(element.textContent).toBe('1 task left');
+        it("it should view should contain in the Document", () => {
+            setUpRenderTodoFooter(1);
+            let element = screen.getByTestId("task_number_indicator");
+            expect(element.textContent).toBe('1 task left');
+        });
     });
 });
